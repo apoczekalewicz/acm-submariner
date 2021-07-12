c61a76d98a58034ed143dadaf58f118866545ce8
ap <ap@laptop.ap.local>
2021-07-12 12:41:36 +0200

    commit2

--- configs/labels.sh
+++
@@ -1,4 +0,0 @@
-oc label managedclusters cluster1 "cluster.open-cluster-management.io/clusterset=submariner" --overwrite
-oc label managedclusters cluster1 "cluster.open-cluster-management.io/submariner-agent=true" --overwrite
-oc label managedclusters cluster2 "cluster.open-cluster-management.io/clusterset=submariner" --overwrite
-oc label managedclusters cluster2 "cluster.open-cluster-management.io/submariner-agent=true" --overwrite
